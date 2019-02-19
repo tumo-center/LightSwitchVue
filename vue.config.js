47f186e43f6ead51ac1ae025db7379bce6a3eb3f
HaykoKoryun <haykokoryun@gmail.com>
2019-02-19 13:17:08 +0400

    update dist paths

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   outputDir: 'docs',
   productionSourceMap: false,
+
   css: {
     extract: false
-  }
+  },
+
+  publicPath: '/LightSwitchVue/',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  parallel: undefined
 }
